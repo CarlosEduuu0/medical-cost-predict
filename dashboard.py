5e3adc115710570b17222aea58a04993c3e57a45
carlos eduardo cardoso silva <carlossilvaaeduuu@gmail.com>
2025-09-25 19:10:08 -0300

    aaaaaaa

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,19 @@
 import dash
-from dash import html, dcc  
-import plotly.express as px
+from dash import html, dcc
 import pandas as pd
-import dash_core_components as dcc
-import dash_html_components as html
+imo     
+import dash_bootstrap_components as dbc  # IMPORT NECESSÁRIO
+from dash_bootstrap_templates import ThemeSwitchAIO
 
-app = dash.Dash(__name__)
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Sample data
-df = pd.read_csv('insrance_data.csv')
+df = pd.read_csv('insurance.csv')
 
+app.layout = dbc.Container([
+    html.H1('opa')
+])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8050)  
